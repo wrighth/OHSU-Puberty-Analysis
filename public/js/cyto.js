d90cb7f3a851a3b1361206762de3cb8e101a4911
Hamzah Khan <khanh@catlin.edu>
2013-08-14 14:48:03 -0700

    Formatting improvements

--- public/js/cyto.js
+++ public/js/cyto.js
@@ -108,16 +108,16 @@ var renderCyto = function renderCyto(cytoVar) {
         var neighborhood = node.neighborhood().add(node);
         var nodeInfo = rgdMap[node.id()]; //change this to use built in data
         
-        msgBox.innerHTML = node.data('id') + ' : ' + node.data('name') + '<br>';
+        msgBox.innerHTML = node.data('id') + "<span class='buffer'></span>";
         
         if(nodeInfo.human) {
-          msgBox.innerHTML += "<a target='_blank' href='" + geneInfoLink + nodeInfo.human.entrezGeneId + "'>Additional Human Information</a><br>";
+          msgBox.innerHTML += "<a target='_blank' href='" + geneInfoLink + nodeInfo.human.entrezGeneId + "'>Additional Human Information</a><span class='buffer'>";
         }
         if(nodeInfo.rat) {
-          msgBox.innerHTML += "<a target='_blank' href='" + geneInfoLink + nodeInfo.rat.entrezGeneId + "'>Additional Rat Information</a><br>";
+          msgBox.innerHTML += "<a target='_blank' href='" + geneInfoLink + nodeInfo.rat.entrezGeneId + "'>Additional Rat Information</a><span class='buffer'>";
         }
         if(nodeInfo.mouse) {
-          msgBox.innerHTML += "<a target='_blank' href='" + geneInfoLink + nodeInfo.mouse.entrezGeneId + "'>Additional Mouse Information</a><br>";
+          msgBox.innerHTML += "<a target='_blank' href='" + geneInfoLink + nodeInfo.mouse.entrezGeneId + "'>Additional Mouse Information</a><span class='buffer'>";
         }
 
         cy.elements().addClass('faded');
